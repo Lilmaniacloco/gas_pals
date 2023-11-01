ba29cd6c134e27a47468f83064a1a62befe579c6
derick <maradiaga.derick@gmail.com>
2023-11-01 12:58:57 -0700

    what i have so far

---
+++ app/src/main/java/com/example/gaspals/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.gaspals;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Clickable(View v){
+
+
+
+    }
+
+
+
+}
